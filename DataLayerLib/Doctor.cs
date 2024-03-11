9b94d5f50b6958193410136fa1b5fd49d0ff4112
nullonspeed <sebastian.langschneider@gmail.com>
2024-03-11 13:07:35 +0100

    f

---
+++ DataLayerLib/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerLib
+{
+    public class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+        public List<Examination> Examinations { get; set; } = new List<Examination>();
+    }
+}
--- DataLayerLib/Examination.cs
+++ DataLayerLib/Examination.cs
@@ -8,5 +8,9 @@ namespace DataLayerLib
 {
     public class Examination
     {
+        public int ExaminationID {  get; set; }
+        public string Description { get; set; }
+
+
     }
 }
